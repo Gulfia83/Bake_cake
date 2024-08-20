70d21d1bc26a01324b28ee5407f3d0a1ab79d799
Stmkv <stepkamakarov1999@mail.ru>
2024-08-20 20:12:52 +0300

    Добавил счетчик переходов по ссылке(Исправление)

--- Bake_cake/urls.py
+++ Bake_cake/urls.py
@@ -3,12 +3,13 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path
 
-from .views import index, lk, lk_order
+from .views import index, lk, lk_order, track_link
 
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', index, name='index'),
-    path('lk', lk, name='lk'),
-    path('lk-order', lk_order, name='lk-order'),
+    path("admin/", admin.site.urls),
+    path("", index, name="index"),
+    path("lk", lk, name="lk"),
+    path("lk-order", lk_order, name="lk-order"),
+    path("click/<int:link_id>/", track_link, name="track_link"),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
